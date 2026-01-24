62d4d08866643c67a1b0fadaf74990e49d78bd57
kartavya <kartavyas@gmail.com>
2026-01-24 12:32:39 +1100

    Person model and DTO added

---
+++ ServiceContracts/DTO/PersonResponse.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// Represents DTO class that is used as return type of most methods of Person Service
+    /// </summary>
+    public class PersonResponse
+    {
+        public Guid PersonID { get; set; }
+        public string? PersonName { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+
+        public Guid? CountryID { get; set; }
+
+        public string? Country { get; set; }
+
+        public string? Address { get; set; }
+
+        public bool ReceiveNewsLetters { get; set; }
+
+        public double? Age { get; set; }
+
+        /// <summary>
+        /// Comapres the current object data with the parameter object
+        /// </summary>
+        /// <param name="obj">THe PersonResponse Onject to compare</param>
+        /// <returns>True of False, including whether all person details are matched with the specified parameter onject</returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            if (obj.GetType() != typeof(PersonResponse)) return false;
+
+            PersonResponse person = (PersonResponse)obj;
+            return 
+                PersonID == person.PersonID && 
+                PersonName == person.PersonName && 
+                Email == person.Email && 
+                DateOfBirth == person.DateOfBirth && 
+                Gender == person.Gender && 
+                CountryID == person.CountryID && 
+                Address == person.Address && 
+                ReceiveNewsLetters == person.ReceiveNewsLetters;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }        
+    }
+    public static class PersonExtension
+    {
+        /// <summary>
+        /// AN extension method to convert an object of Person calss into PersonResponse class
+        /// </summary>
+        /// <param name="person">The Person object to convert</param>
+        /// <returns>Returns the converted PersonResponse object</returns>
+        public static PersonResponse ToPersonResponse(this Person person)
+        {
+            return new PersonResponse()
+            {
+                PersonID = person.PersonID,
+                PersonName = person.PersonName,
+                Email = person.Email,
+                DateOfBirth = person.DateOfBirth,
+                ReceiveNewsLetters = person.ReceiveNewsLetters,
+                Address = person.Address,
+                CountryID = person.CountryID,
+                Gender = person.Gender,
+                Age = (person.DateOfBirth != null) ? Math.Round((DateTime.Now - person.DateOfBirth.Value).TotalDays /365.25) : null
+            };
+        }
+
+    }
+}
