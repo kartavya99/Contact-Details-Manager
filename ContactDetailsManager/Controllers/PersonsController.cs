b576b5ae1c5c7b3e01d3d66e23497dfb114f75a0
kartavya <kartavyas@gmail.com>
2026-01-26 16:03:37 +1100

    Basic set up to get started with UI

---
+++ ContactDetailsManager/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactDetailsManager.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
